4ae7f0c4cfc030a4c07e543f5c0cacda93fc6b91
Anjuman Hasan <anju2084@gmail.com>
2021-07-31 14:41:54 +0530

    login and signup activities

--- app/src/main/java/com/example/fitme/MainActivity.java
+++ app/src/main/java/com/example/fitme/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.fitme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+Button loginbutton1,loginbutton2,signupbutton1,signupbutton2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.startpage);
+loginbutton1 = findViewById(R.id.loginbutton1);
+loginbutton2= findViewById(R.id.loginbutton2);
+signupbutton1=findViewById(R.id.signupbutton1);
+signupbutton2=findViewById(R.id.signupbutton2);
+
+
+//login and signup
+loginbutton1.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        openLoginActivity();
     }
+});
+signupbutton1.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        openSignupActivity();
+    }
+});
+
+    }
+    public void openLoginActivity()
+    {
+        Intent intent = new Intent(this,loginactivity.class);
+        startActivity(intent);
+    }
+    public void openSignupActivity()
+    {
+        Intent intent = new Intent(this,signup.class);
+        startActivity(intent);
+    }
+
+    //login and signup
 }
