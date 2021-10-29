3e134dc59b9e24cfa449931aa0cd470a564609d8
Anjuman Hasan <anju2084@gmail.com>
2021-10-29 17:45:11 +0530

    Further UI Changes

---
+++ app/src/main/java/com/example/fitme/genderselection.java
@@ -0,0 +1,13 @@
+package com.example.fitme;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class genderselection extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_genderselection);
+    }
+}
